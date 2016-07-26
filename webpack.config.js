8c1f0dbcd54a795bc4faa7d7e0beacea43a93bd5
YOUR FULL NAME <farishta.haider@gmail.com>
2016-07-26 15:31:57 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,3 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + "/app/index.html",
-  filename: "index.html",
-  inject: "body"
-});
-
 module.exports = {
   entry: './main.js',
   output: {
@@ -26,8 +19,5 @@ module.exports = {
         }
       }
     ]
-  },
-    plugins: [
-    HtmlWebpackPluginConfig
-  ]
+  }
 }
